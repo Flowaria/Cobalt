6ad677d6563f3ab44d6f08e89ac0b66f84d9397e
ratios <ratios@DESKTOP-PR58D71>
2017-01-26 22:01:52 +0900

    Enum 추가 Hidden 필드 추가

--- Cobalt/Src/MvM/Items/TFAttribute.cs
+++ Cobalt/Src/MvM/Items/TFAttribute.cs
@@ -2,6 +2,18 @@
 {
     public class TFAttribute : TFItemInstance
     {
+        public enum EffectSide
+        {
+            Positive, Negative, Neutral
+        }
+        public enum DescriptionFormat
+        {
+            Percentage, Inverted_Percentage,
+            Additive, Additive_Percentage,
+            Date, Or, From_Lookup_Table, Particle_Index,
+            KillstreakEffect_Index, Killstreak_IdleEffect_Index
+        }
         public string Description;
+        public bool Hidden;
     }
 }
